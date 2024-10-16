021ee982913b6596b6a66672ed8e93bddc665e56
nhtfw <huuthangfw@gmail.com>
2024-10-17 01:41:11 +0700

    toi khuya sang 16 17/10

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -19,7 +19,26 @@ public class EmailController {
     @ApiMessage("Send simple email")
     public String sendSimpleEmail() {
 
-        this.emailService.sendSimpleEmail();
+        // this.emailService.sendSimpleEmail();
+
+        // this.emailService.sendEmailSync("odinkun20303@gmail.com", "Test send email",
+        // "<h1> <b> Hello </b> </h1>", false, true);
+
+        // "job" -> file job.html trong folder resources/templates
+        /*
+         * Vì sao dự án có thể hiểu job là job.html và phải tìm job.html trong folder
+         * templates ? => Vì đó là cấu hình mặc định của java spring với thymeleaf, nếu
+         * khai báo 1 file ở ngoài templates thì sẽ không chạy được vì không tìm thấy
+         * bên trong folder templates
+         */
+        this.emailService.sendEmailFromTemplateSync("odinkun20303@gmail.com", "Test send email", "job");
+
+        /*- Code HTML và style CSS trong cùng 1 file (không code riêng lẻ HTML và css) => sử
+        dụng css với tag <style> </style>
+        - Convert HTML/CSS => inline CSS
+        - Mặc định Gmail sẽ bỏ qua CSS ở phần header => để đảm bảo an toàn hơn cho người
+        dùng (tránh nhúng link javascript ở header)
+        - Nên sử dụng layout table (không dùng css flex) */
 
         return "ok";
     }
--- src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -1,15 +1,31 @@
 package vn.hoidanit.jobhunter.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailService {
 
     @Autowired
     private MailSender mailSender;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private SpringTemplateEngine springTemplateEngine;
 
     public void sendSimpleEmail() {
 
@@ -23,4 +39,35 @@ public class EmailService {
 
         this.mailSender.send(msg);
     }
+
+    // gửi email đồng bộ (Synchronize)
+    public void sendEmailSync(String to, String subject, String content, boolean isMultipart,
+            boolean isHtml) {
+        // Prepare message using a Spring helper
+        MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
+
+        // isMultipart: gửi kèm hình ảnh, file,... hay không
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, StandardCharsets.UTF_8.name());
+            message.setTo(to);
+            message.setSubject(subject);
+
+            // nội dung mail: nếu isHtml == true thì nội dung sẽ là dưới dạng html
+            message.setText(content, isHtml);
+            this.javaMailSender.send(mimeMessage);
+        } catch (MailException | MessagingException e) {
+            System.out.println("ERROR SEND EMAIL: " + e);
+        }
+    }
+
+    public void sendEmailFromTemplateSync(String to, String subject, String templateName) {
+        Context context = new Context();
+        // TemplateEngine convert từ file html -> text
+        // đầu vào là file html. đầu ra là string
+        String content = this.springTemplateEngine.process(templateName, context);
+
+        // sau đó gửi mail bằng hàm ở trên
+        this.sendEmailSync(to, subject, content, false, true);
+    }
+
 }
